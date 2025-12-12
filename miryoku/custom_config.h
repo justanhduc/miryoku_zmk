542aa2fea279bcd9a5e006cfe9143619d462b3ec
Duc Nguyen <justanhduc@users.noreply.github.com>
2025-12-12 11:25:23 +0700

    Reorder key definitions in MIRYOKU_LAYER_NAV

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -41,7 +41,7 @@ MIRYOKU_X(FUN,    "Fun")
                 U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
                 &kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,              &kp LEFT,          &kp DOWN,          &kp UP,            &kp RIGHT,         &u_caps_word,      \
                 U_NA,              &kp RALT,          &u_to_U_NUM,       &u_to_U_NAV,       U_NA,              &kp HOME,          &kp PG_DN,         &kp PG_UP,         &kp END,           &kp INS,           \
-                U_NP,              U_NP,              U_NP,              U_NP,              U_NP,              U_NP,              &kp RET,           &kp BSPC,          &kp DEL,           U_NP
+                U_NP,              U_NP,              U_NP,              U_NP,              U_NP,              U_NP,              &kp BSPC,          &kp RET,           &kp DEL,           U_NP
 
 #define MIRYOKU_LAYER_MOUSE \
                 U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
